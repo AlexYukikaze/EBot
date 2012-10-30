2504d7fabd6be5b1b5e313c75b92cb878efb41fd
EveSuper <evesupernerf@gmail.com>
2012-10-30 07:04:55 +0100

    Redone, my mistake

---
+++ EveModel/EveAgentDialogWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveModel
+{
+    public class EveAgentDialogWindow : EveWindow
+    {
+        public string Briefing
+        {
+            get { return this["sr"]["briefingBrowser"]["sr"]["currentTXT"].GetValueAs<String>(); }
+        }
+        public string Objective
+        {
+            get { return this["sr"]["objectiveBrowser"]["sr"]["currentTXT"].GetValueAs<String>(); }
+        }
+        public int AgentId
+        {
+            get { return this["sr"]["agentID"].GetValueAs<int>(); }
+        }
+    }
+}
